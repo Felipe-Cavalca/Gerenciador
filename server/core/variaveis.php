9f7740895014a0088cf5871e57ebaf85f60c16e3
Felipe-Cavalca <felipe.cavalca@tmh.com.br>
2021-10-14 22:04:40 -0300

    Criação do arquivo de variaveis php

---
+++ server/core/variaveis.php
@@ -0,0 +1,18 @@
+<?php
+
+//urls
+$_UrlApp = "http://gerenciador/v2/app/";
+$_UrlServer = "http://gerenciador/v2/server/";
+$_UrlWebhookBanco = "sua_url";
+
+//nomes
+$_AppNome = "gerenciador";
+$_NomeBootDefault = "Boot ".$_AppNome;
+$_NomeBootBanco = "Boot Banco ".$_AppNome;
+
+//Banco
+$_HostBanco = 'host';
+$_NomeBanco = 'nome';
+$_UsuarioBanco = 'usuario';
+$_SenhaBanco = 'senha';
+?>
