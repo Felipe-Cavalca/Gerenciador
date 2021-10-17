f67c37ad7dfda70b762a1184e024ed7db6ab9627
Felipe-Cavalca <felipe.cavalca@tmh.com.br>
2021-10-17 17:32:05 -0300

    Criação do sistema de login
    Signed-off-by: Felipe-Cavalca <felipe.cavalca@tmh.com.br>

---
+++ app/js/deslogado/login.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $("#login").submit(function(event) {
+
+        //para não fazer o submit do form
+        event.preventDefault();
+
+        //altera as classes dos botôes para fazer as animações
+        $("#btnLogin").removeClass("scale-in");
+        $("#btnLogin").addClass("scale-out");
+        $("#carregando").removeClass("scale-out");
+        $("#carregando").addClass("scale-in");
+
+        //envia os dados e agurda a resp
+        $.ajax({
+            url : _UrlServer+"login.php", //!_URL
+            type : 'post',
+            data : {
+                email : $('#email').val(),
+                senha : $('#senha').val(),
+            },
+            beforeSend : function(){
+            }
+        })
+        .done(function(msg){
+            setTimeout(function () {
+                if(isJson(msg)){
+                    dados = JSON.parse(msg);
+                    if(dados['status'] == true){
+                        toastSucesso("Login concluido");
+                        setTimeout(function (){
+                            window.location.href = "../home/home.html"; //!_URL
+                        }, 2000);
+                    }else{
+                        toastErro(dados['msg']);
+                    }    
+                }else{
+                    toastErroDesconhecido();
+                }
+            },1500);
+        })
+        .fail(function(){
+            toastErro("Não foi possivel localizar o servidor!");
+        });
+        
+        setTimeout(function () {
+            //altera as classes dos botoes para fazer animações
+            //altera as classes dos botôes para fazer as animações
+            $("#btnLogin").removeClass("scale-out");
+            $("#btnLogin").addClass("scale-in");
+            $("#carregando").removeClass("scale-in");
+            $("#carregando").addClass("scale-out");
+        }, 1000)
+    });
+});
---
+++ server/login.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'core/variaveis.php';
+include 'core/funcoes.php';
+include 'core/msgDiscord.php';
+include 'core/banco.php';
+
+extract($_POST);
+
+$query =[
+    'tabela' => 'usuario',
+    'igual' =>[
+        'email' => $email,
+        'senha' => hash('sha512', $senha)
+    ],
+    'contar' => true
+];
+
+if(select($query) == 1){
+    $retorno= [
+        'status' => true,
+        'msg' => "Login efetuado com sucesso"
+    ];
+}else{
+    $retorno= [
+        'status' => false,
+        'msg' => "Email ou senha invalidos"
+    ];
+}
+
+
+echo json_encode($retorno, true);
