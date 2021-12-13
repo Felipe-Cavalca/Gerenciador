9077fa6c5e470ba0b2ee75004ac76e5a9310f6b2
Felipe-Cavalca <felipe.cavalca@tmh.com.br>
2021-12-13 17:01:46 -0300

    faz tanto tempo q não sei oq tava fznd
    Signed-off-by: Felipe-Cavalca <felipe.cavalca@tmh.com.br>

--- web/js/cadastro.js
+++ web/js/cadastro.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
         $("#carregando-inventario").removeClass("scale-out");
         $("#carregando-inventario").addClass("scale-in");
 
-
         //envia os dados e agurda a resp
         $.ajax({
             url: _UrlServer + 'inventario/cadastro.php',
@@ -22,39 +21,93 @@ $(document).ready(function () {
             beforeSend: function () {
             }
         })
-            .done(function (msg) {
-                //seta um tempo apos a resposta
-                setTimeout(function () {
-                    //caso sucesso
-                    if (msg['status'] == true) {
-                        //exibe o toast e chama a função de listar inventario
-                        toastSucesso("Cadastro concluido");
-                        listarInventarios('tabelaIventarios', 'carregandoTabelaInventarios', 'selecionaInventario()');
+        .done(function (msg) {
+            //seta um tempo apos a resposta
+            setTimeout(function () {
+                //caso sucesso
+                if (msg['status'] == true) {
+                    //exibe o toast e chama a função de listar inventario
+                    toastSucesso("Cadastro concluido");
+                    listarInventarios('tabelaIventarios', 'carregandoTabelaInventarios', 'selecionaInventario()');
+                } else {
+                    //caso erro verifica se está logado
+                    if (!msg['logado']) {
+                        deslogar();
                     } else {
-                        //caso erro verifica se está logado
-                        if (!msg['logado']) {
-                            deslogar();
-                        } else {
-                            toastErro(msg['msg']);
-                        }
+                        toastErro(msg['msg']);
                     }
-                    //aguarda um tempo para não ficar esquisito
-                    setTimeout(function () {
-                        //altera as classes dos botoes para fazer animações
-                        //altera as classes dos botôes para fazer as animações
-                        $("#btn-cadastro-inventario").removeClass("scale-out");
-                        $("#btn-cadastro-inventario").addClass("scale-in");
-                        $("#carregando-inventario").removeClass("scale-in");
-                        $("#carregando-inventario").addClass("scale-out");
-                    }, 1000)
-                }, 1500);
-            })
-            .fail(function () {
-                //caso a reuisição falhe
-                toastErro("Não foi possivel localizar o servidor!");
-            });
+                }
+                //aguarda um tempo para não ficar esquisito
+                setTimeout(function () {
+                    //altera as classes dos botoes para fazer animações
+                    //altera as classes dos botôes para fazer as animações
+                    $("#btn-cadastro-inventario").removeClass("scale-out");
+                    $("#btn-cadastro-inventario").addClass("scale-in");
+                    $("#carregando-inventario").removeClass("scale-in");
+                    $("#carregando-inventario").addClass("scale-out");
+                }, 1000)
+            }, 1500);
+        })
+        .fail(function () {
+            //caso a reuisição falhe
+            toastErro("Não foi possivel localizar o servidor!");
+        });
     });
 
+    $("#form-categoria").submit(function (event) {
+
+        //para não fazer o submit do form
+        event.preventDefault();
+
+        //altera as classes dos botôes para fazer as animações
+        $("#btn-cadastro-categoria").removeClass("scale-in");
+        $("#btn-cadastro-categoria").addClass("scale-out");
+        $("#carregando-categoria").removeClass("scale-out");
+        $("#carregando-categoria").addClass("scale-in");
+
+        //envia os dados e agurda a resp
+        $.ajax({
+            url: _UrlServer + 'categoria/cadastro.php',
+            type: 'post',
+            data: {
+                nome: $('#nome-categoria').val(),
+                categoriaProdutos: $('#categoria-produtos').val(),
+            },
+            beforeSend: function () {
+            }
+        })
+        .done(function (msg) {
+            //seta um tempo apos a resposta
+            setTimeout(function () {
+                //caso sucesso
+                if (msg['status'] == true) {
+                    //exibe o toast e chama a função de listar categoria
+                    toastSucesso("Cadastro concluido");
+                    // listarInventarios('tabelaIventarios', 'carregandoTabelaInventarios', 'selecionaInventario()');
+                } else {
+                    //caso erro verifica se está logado
+                    if (!msg['logado']) {
+                        deslogar();
+                    } else {
+                        toastErro(msg['msg']);
+                    }
+                }
+                //aguarda um tempo para não ficar esquisito
+                setTimeout(function () {
+                    //altera as classes dos botoes para fazer animações
+                    //altera as classes dos botôes para fazer as animações
+                    $("#btn-cadastro-categoria").removeClass("scale-out");
+                    $("#btn-cadastro-categoria").addClass("scale-in");
+                    $("#carregando-categoria").removeClass("scale-in");
+                    $("#carregando-categoria").addClass("scale-out");
+                }, 1000)
+            }, 1500);
+        })
+        .fail(function () {
+            //caso a reuisição falhe
+            toastErro("Não foi possivel localizar o servidor!");
+        });
+    });
 });
 
 
@@ -83,17 +136,12 @@ function selecionaInventario() {
             //some com o botão de cadastro
             $("#btn-cadastro-inventario").hide();
             //exibe os botões
-            $("#btn-editar-inventario").show();
-            $("#btn-excluir-inventario").show();
-            $("#btn-cancelar-inventario").show();
-            $("#btn-editar-inventario").removeClass("scale-out");
-            $("#btn-editar-inventario").addClass("scale-in");
-            $("#btn-excluir-inventario").removeClass("scale-out");
-            $("#btn-excluir-inventario").addClass("scale-in");
-            $("#btn-cancelar-inventario").removeClass("scale-out");
-            $("#btn-cancelar-inventario").addClass("scale-in");
-            $("#indicacao-edicao-inventario").removeClass("scale-out");
-            $("#indicacao-edicao-inventario").addClass("scale-in");
+            $(".edicao-inventario").show();
+            $(".edicao-inventario").removeClass("scale-out");
+            $(".edicao-inventario").addClass("scale-in");
+            $(".indicacao-edicao-inventario").show();
+            $(".indicacao-edicao-inventario").removeClass("scale-out");
+            $(".indicacao-edicao-inventario").addClass("scale-in");
         }, 300);
     });
 }
@@ -103,23 +151,17 @@ function edicaoInventarioHide() {
     $("#carregando-inventario").removeClass("scale-in");
     $("#carregando-inventario").addClass("scale-out");
 
-    $("#btn-editar-inventario").removeClass("scale-in");
-    $("#btn-editar-inventario").addClass("scale-out");
-    $("#btn-excluir-inventario").removeClass("scale-in");
-    $("#btn-excluir-inventario").addClass("scale-out");
-    $("#btn-cancelar-inventario").removeClass("scale-in");
-    $("#btn-cancelar-inventario").addClass("scale-out");
-    $("#indicacao-edicao-inventario").removeClass("scale-in");
-    $("#indicacao-edicao-inventario").addClass("scale-out");
+    $(".edicao-inventario").removeClass("scale-in");
+    $(".edicao-inventario").addClass("scale-out");
+    $(".indicacao-edicao-inventario").removeClass("scale-in");
+    $(".indicacao-edicao-inventario").addClass("scale-out");
 
     setTimeout(function () {
         //esconde os botões
-        $("#btn-editar-inventario").hide();
-        $("#btn-excluir-inventario").hide();
-        $("#btn-cancelar-inventario").hide();
+        $(".edicao-inventario").hide();
         //mostra o botão de cadastro
         $("#btn-cadastro-inventario").show();
-        //remove o botão de cadastro
+        //adciona o botão de cadastro
         $("#btn-cadastro-inventario").removeClass("scale-out");
         $("#btn-cadastro-inventario").addClass("scale-in");
     }, 300);
@@ -134,12 +176,10 @@ function edicaoInventarioHide() {
 
 function modificarInventario(tipo) {
     //altera as classes dos botôes para fazer as animações
-    $("#btn-editar-inventario").removeClass("scale-in");
-    $("#btn-editar-inventario").addClass("scale-out");
-    $("#btn-excluir-inventario").removeClass("scale-in");
-    $("#btn-excluir-inventario").addClass("scale-out");
-    $("#btn-cancelar-inventario").removeClass("scale-in");
-    $("#btn-cancelar-inventario").addClass("scale-out");
+    $(".edicao-inventario").removeClass("scale-in");
+    $(".edicao-inventario").addClass("scale-out");
+    $(".indicacao-edicao-inventario").removeClass("scale-in");
+    $(".indicacao-edicao-inventario").addClass("scale-out");
     $("#carregando-inventario").removeClass("scale-out");
     $("#carregando-inventario").addClass("scale-in");
 
@@ -154,36 +194,36 @@ function modificarInventario(tipo) {
                 nome: $('#nome-inventario').val(),
             }
         })
-            .done(function (msg) {
-                //seta um tempo apos a resposta
-                setTimeout(function () {
-                    //caso sucesso
-                    if (msg['status'] == true) {
-                        //exibe o toast e chama a função de listar inventario
-                        toastSucesso("Inventario editado");
-                        listarInventarios('tabelaIventarios', 'carregandoTabelaInventarios', 'selecionaInventario()');
+        .done(function (msg) {
+            //seta um tempo apos a resposta
+            setTimeout(function () {
+                //caso sucesso
+                if (msg['status'] == true) {
+                    //exibe o toast e chama a função de listar inventario
+                    toastSucesso("Inventario editado");
+                    listarInventarios('tabelaIventarios', 'carregandoTabelaInventarios', 'selecionaInventario()');
+                } else {
+                    //caso erro verifica se está logado
+                    if (!msg['logado']) {
+                        deslogar();
                     } else {
-                        //caso erro verifica se está logado
-                        if (!msg['logado']) {
-                            deslogar();
-                        } else {
-                            toastErro(msg['msg']);
-                        }
+                        toastErro(msg['msg']);
                     }
-                    //aguarda um tempo para não ficar esquisito
-                    setTimeout(function () {
-                        edicaoInventarioHide();
-                    }, 1000)
-                }, 1500);
-            })
-            .fail(function () {
-                //caso a reuisição falhe
-                toastErro("Não foi possivel localizar o servidor!");
+                }
                 //aguarda um tempo para não ficar esquisito
                 setTimeout(function () {
                     edicaoInventarioHide();
-                }, 5000)
-            });
+                }, 1000)
+            }, 1500);
+        })
+        .fail(function () {
+            //caso a reuisição falhe
+            toastErro("Não foi possivel localizar o servidor!");
+            //aguarda um tempo para não ficar esquisito
+            setTimeout(function () {
+                edicaoInventarioHide();
+            }, 5000)
+        });
     } else if (tipo == "desativar" || tipo == 'excluir') {
         //envia os dados e agurda a resp
         $.ajax({
